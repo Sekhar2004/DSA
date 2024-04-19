f33130d7a9ad77f1fb943958fcb23e91ea4a09cf
Sekhar Reddy <131131670+Sekhar2004@users.noreply.github.com>
2024-04-19 23:26:34 +0530

    Update Fruitsintobaskets.cpp

--- Fruitsintobaskets.cpp
+++ Fruitsintobaskets.cpp
@@ -1,1 +1,32 @@
+//sliding window
+//my map hash map is uses here inorder to count the frequency of every fruit type....
+
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+int findMaxFruits(std::vector<int>& arr, int n) 
+{
+    int left = 0, right = 0, maxLen = 0;
+    std::unordered_map<int, int> mymap;
+
+    while (right < n)
+     {
+         mymap[arr[right]]++;
+        if (mymap.size() > 2)
+         {
+            --mymap[arr[left]];
+            if (mymap[arr[left]] == 0) 
+            {
+                mymap.erase(arr[left]);
+            }
+            ++left;
+        }
+
+        maxLen = std::max(maxLen, right - left + 1);
+        ++right;
+    }
+
+    return maxLen;
+}
 
